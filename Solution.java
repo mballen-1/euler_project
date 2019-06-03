81704795f1b18052bffb15cdee4e5dc4379ba695
Miguel Ballén <migaballengal@unal.edu.co>
2019-06-03 00:21:26 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,5 +1,5 @@
-//This code wont even compile, still working on that (Going to sleep, lol)
 
+// This codes throws nicely an stack overflow exception. Working on that later! :P
 import java.io.*;
 import java.util.*;
 import java.math.BigInteger;
@@ -9,7 +9,7 @@ public class Solution {
     short b;
     short u_0;
 
-    public double f(int x) {
+    public double f(double x) {
         return 1/(Math.floor(Math.pow(2, this.b - Math.pow(x, 2)))) * Math.pow(10, 9);
     }
 
@@ -26,7 +26,7 @@ public class Solution {
         x.b = lec.nextShort();
         x.u_0 = lec.nextShort();
         float n = (int) Math.pow(10, 12);
-        float ans = un(n) + un(n+1);        
+        double ans = x.un(n) + x.un(n+1);        
         System.out.println(ans);
     }
 }

d0a1abf6e13b4a3b9db0f7e28672b881c0227bbb
Miguel Ballén <migaballengal@unal.edu.co>
2019-06-03 00:17:27 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,29 +1,31 @@
+//This code wont even run correcty, still working on that (Going to sleep, lol)
+
 import java.io.*;
 import java.util.*;
 import java.math.BigInteger;
 
 public class Solution {
 
-    
+    short b;
+    short u_0;
+
+    public double f(int x) {
+        return 1/(Math.floor(Math.pow(2, this.b - Math.pow(x, 2)))) * Math.pow(10, 9);
+    }
+
+    public double un(double x) {
+        if(x == 0){
+            return this.u_0;
+        }
+        return f(un(--x));
+    }
+
     public static void main(String[] args) {
+        Solution x = new Solution();
         Scanner lec = new Scanner(System.in);
-        short b;
-        short u_0;
-
-        public double f(int x) {
-            return 1/(Math.floor(Math.pow(2, b - Math.pow(x, 2)))) * Math.pow(10, 9);
-        }
-
-        public double un(int x) {
-            if(x == 0){
-                return u_0;
-            }
-            return f(un(--x));
-        }
-
-        b = lec.nextShort();
-        u_0 = lec.nextShort();
-        Integer n = (int) Math.pow(10, 12);
+        x.b = lec.nextShort();
+        x.u_0 = lec.nextShort();
+        float n = (int) Math.pow(10, 12);
         float ans = un(n) + un(n+1);        
         System.out.println(ans);
     }
